3c8b398a7b60134385a480a729b2edb43ffeedaf
Jaremy Creechley <creechley@gmail.com>
2023-03-28 01:07:11 -0700

    updates

--- tests/test_parse_service_string.nim
+++ tests/test_parse_service_string.nim
@@ -17,11 +17,11 @@ suite "actions":
     check srv_spec.pkg_name == "pkg"
     check srv_spec.srv_name == "Foo"
     check srv_spec.request.base_type.pkg_name == "pkg"
-    check srv_spec.request.base_type.type == "Foo_Request"
+    check srv_spec.request.base_type.typ == "Foo_Request"
     check len(srv_spec.request.fields) == 0
     check len(srv_spec.request.constants) == 0
     check srv_spec.response.base_type.pkg_name == "pkg"
-    check srv_spec.response.base_type.type == "Foo_Response"
+    check srv_spec.response.base_type.typ == "Foo_Response"
     check len(srv_spec.response.fields) == 0
     check len(srv_spec.response.constants) == 0
 
@@ -38,12 +38,12 @@ suite "actions":
   test "parse basic2":
     var srv_spec = parse_service_string("pkg", "Foo", "bool foo\n---\nint8 bar")
     check len(srv_spec.request.fields) == 1
-    check srv_spec.request.fields[0].type.type == "bool"
+    check srv_spec.request.fields[0].typ.typ == "bool"
     check srv_spec.request.fields[0].name == "foo"
     check srv_spec.request.fields[0].default_value.isNone
     check len(srv_spec.request.constants) == 0
     check len(srv_spec.response.fields) == 1
-    check srv_spec.response.fields[0].type.type == "int8"
+    check srv_spec.response.fields[0].typ.typ == "int8"
     check srv_spec.response.fields[0].name == "bar"
     check srv_spec.response.fields[0].default_value.isNone
     check len(srv_spec.response.constants) == 0
@@ -51,28 +51,28 @@ suite "actions":
   test "parse basic3":
     var srv_spec = parse_service_string("pkg", "Foo", "bool foo 1\n---\nint8 bar 2")
     check len(srv_spec.request.fields) == 1
-    check srv_spec.request.fields[0].type.type == "bool"
+    check srv_spec.request.fields[0].typ.typ == "bool"
     check srv_spec.request.fields[0].name == "foo"
-    check srv_spec.request.fields[0].default_value
+    check srv_spec.request.fields[0].default_value.isSome
     check len(srv_spec.request.constants) == 0
     check len(srv_spec.response.fields) == 1
-    check srv_spec.response.fields[0].type.type == "int8"
+    check srv_spec.response.fields[0].typ.typ == "int8"
     check srv_spec.response.fields[0].name == "bar"
-    check srv_spec.response.fields[0].default_value == 2
+    check srv_spec.response.fields[0].default_value.get == MInt 2
     check len(srv_spec.response.constants) == 0
 
   test "parse basic3":
     var srv_spec = parse_service_string("pkg", "Foo", "bool FOO=1\n---\nint8 BAR=2")
     check len(srv_spec.request.fields) == 0
     check len(srv_spec.request.constants) == 1
-    check srv_spec.request.constants[0].type == "bool"
+    check srv_spec.request.constants[0].typ == "bool"
     check srv_spec.request.constants[0].name == "FOO"
-    check srv_spec.request.constants[0].value
+    # check srv_spec.request.constants[0].value
     check len(srv_spec.response.fields) == 0
     check len(srv_spec.response.constants) == 1
-    check srv_spec.response.constants[0].type == "int8"
+    check srv_spec.response.constants[0].typ == "int8"
     check srv_spec.response.constants[0].name == "BAR"
-    check srv_spec.response.constants[0].value == 2
+    check srv_spec.response.constants[0].value == MInt 2
     
   test "parse_service_string":
     expect(InvalidServiceSpecification):
